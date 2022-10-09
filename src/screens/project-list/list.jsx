3f8973390e9700fe605a031101ff04aba1c3073b
w-kuiq <569271010@qq.com>
2022-10-09 09:53:58 +0800

    feat: 新增列表

---
+++ src/screens/project-list/list.jsx
@@ -0,0 +1,22 @@
+
+import { React } from "react";
+
+export const List = ({users,list})=>{
+  return <table>
+    <thead>
+      <tr>
+        <th>名称</th>
+        <th>负责人</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        list.map(item=>
+        <tr key={item.id}>
+          <td>{item.name}</td>
+          <td>{users.find(user=>user.id===item.personId)?.name}</td>
+        </tr>)
+      }
+    </tbody>
+  </table>
+}
